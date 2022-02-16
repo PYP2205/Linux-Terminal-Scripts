f2fa92f45fae259369e62cc22bbdf8a5a45a5fc8
Paramon, Yevstigneyev <90145033+ParamonPlay2205@users.noreply.github.com>
2022-02-16 07:41:40 -0800

    Create install_programs.sh
    Helpful for when you are installing Linux programs with file .deb files.

---
+++ install_programs.sh
@@ -0,0 +1,5 @@
+# Linux shell script that installs .deb (debian) files to your system.
+# Note: This only works for Debain, and Debian based systems (Ubuntu, Kali Linux, etc.).
+
+# Installs all the .deb files that are in the same direcotry as this script.
+sudo dkpg --install *.deb
