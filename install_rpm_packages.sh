23ced2c476e3d89f63058f4edf54d0d9acfc4b24
Paramon, Yevstigneyev <90145033+ParamonPlay2205@users.noreply.github.com>
2022-03-25 12:22:54 -0700

    Create install_rpm_packages.sh

---
+++ install_rpm_packages.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Linux Shell script tht installs RPM files in the local directory
+# Works in Red Hat or Red Hat Based systems (ex. Fedora) only.
+
+# Installs all RPM files that are located in the direcotry the script is in
+sudo rpm install *.rpm
